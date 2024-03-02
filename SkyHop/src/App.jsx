aaa81fc97b7676fff80c2479a02477bd46d4b391
jobindnl <jobindnl@gmail.com>
2024-03-01 22:34:29 -0500

    css styling. font change and color changes. adds react-modal

--- SkyHop/src/App.jsx
+++ SkyHop/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
-    <div>
+    <div className='entry-point'>
+      Click to see SkyHop Modal
     </div>
   )
 }
