99ece66aae8620ba34f5a26e43c1b6221369d3b8
jobindnl <jobindnl@gmail.com>
2024-06-18 01:03:00 -0400

    makes more skeleton changes in formatting. left and right main body mocked

--- SkyHop/src/Components/Body.jsx
+++ SkyHop/src/Components/Body.jsx
@@ -1,8 +1,19 @@
+import Footer from "./Footer"
+
 const Body = () => {
 
   return (
     <div className="main-body">
       <p className="title" >Document Upload</p>
+      <div className="logic-body">
+        <div className="left">
+          <p>testing</p>
+        </div>
+        <div className="right">
+          <p>testing</p>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ SkyHop/src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p className="footer-title">Data in the import file is correct. Please press Continue to import</p>
+    </div>
+  )
+}
+
+export default Footer
