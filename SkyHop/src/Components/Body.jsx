f6fd57be4b45f5c3d3ba507ac6fd8d2bdacc882b
jobindnl <jobindnl@gmail.com>
2024-06-18 01:27:35 -0400

    converted dropdown to react component

--- SkyHop/src/Components/Body.jsx
+++ SkyHop/src/Components/Body.jsx
@@ -1,4 +1,5 @@
 import Footer from "./Footer"
+import DropdownNameInput from "./DropdownNameInput"
 
 const Body = () => {
 
@@ -6,16 +7,7 @@ const Body = () => {
     <div className="main-body">
       <p className="title" >Document Upload</p>
       <div className="logic-body">
-        <div className="left">
-          <div className="select">
-            <select>
-              <option selected disabled>Select Import Name:</option>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
-            </select>
-          </div>
-        </div>
+        <DropdownNameInput />
         <div className="right">
           <p>testing</p>
         </div>
