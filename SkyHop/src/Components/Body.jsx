ba496e4afcd9461a1fa5f7b04a41121f92428ec0
jobindnl <jobindnl@gmail.com>
2024-06-18 01:21:48 -0400

    adds dropdown

--- SkyHop/src/Components/Body.jsx
+++ SkyHop/src/Components/Body.jsx
@@ -7,7 +7,14 @@ const Body = () => {
       <p className="title" >Document Upload</p>
       <div className="logic-body">
         <div className="left">
-          <p>testing</p>
+          <div className="select">
+            <select>
+              <option selected disabled>Select Import Name:</option>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
         </div>
         <div className="right">
           <p>testing</p>
