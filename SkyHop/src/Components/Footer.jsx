73a816667aea605be6f7cbeb1d9b68a785b2fbf2
jobindnl <jobindnl@gmail.com>
2024-06-25 02:09:27 -0400

    created footer component with button styling

--- SkyHop/src/Components/Footer.jsx
+++ SkyHop/src/Components/Footer.jsx
@@ -2,6 +2,10 @@ const Footer = () => {
   return (
     <div>
       <p className="footer-title">Data in the import file is correct. Please press Continue to import</p>
+      <div className="button-wrapper">
+        <button className="button-continue">Continue Import</button>
+        <button className="button-cancel">Cancel</button>
+      </div>
     </div>
   )
 }
