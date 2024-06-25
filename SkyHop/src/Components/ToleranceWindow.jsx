d81f8531a877beb78a5914121027a6845d571ba5
jobindnl <jobindnl@gmail.com>
2024-06-25 02:03:34 -0400

    toggle component styling changes

--- SkyHop/src/Components/ToleranceWindow.jsx
+++ SkyHop/src/Components/ToleranceWindow.jsx
@@ -1,9 +1,12 @@
 const ToleranceWindow = () => {
   return (
-    <div className="tolerance-window">
-      <input type="checkbox" id="switch" class="checkbox" />
-      <label for="switch" class="toggle" />
-      <p className="toggle-state">Toggle ON</p>
+    <div className="tolerance-window-wrapper">
+      <p className="tolerance-window-title">Tolerance Window:</p>
+      <div className="toggle-wrapper">
+        <input type="checkbox" id="switch" class="checkbox" />
+        <label for="switch" class="toggle" />
+        <p className="toggle-state">Toggle ON</p>
+      </div>
     </div>
   )
 }
