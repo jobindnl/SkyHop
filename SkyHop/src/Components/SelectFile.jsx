b9864b5d0aa77c9b3fa9a241f89ebc9f7281df26
jobindnl <jobindnl@gmail.com>
2024-06-24 23:11:55 -0400

    added image to drop-zone

--- SkyHop/src/Components/SelectFile.jsx
+++ SkyHop/src/Components/SelectFile.jsx
@@ -1,3 +1,5 @@
+import fileImage from "./../assets/fileImage.png"
+
 const SelectFile = () => {
   const dropHandler = (e) => {
     e.preventDefault()
@@ -11,11 +13,14 @@ const SelectFile = () => {
     <div className="select-file">
       <p className="select-file-title">Select a manifest you would like to import</p>
       <div className="drop-zone-wrapper">
-        <div
-          id="drop-zone"
-          onDrop={dropHandler}
-          onDragOver={dragOverHandler}>
-          <p className="drop-zone-description">Drag & Drop Here Or <b>Browse</b></p>
+        <div className="drop-zone-border">
+          <img src={fileImage} className="drop-zone-image" alt="file-icon" />
+          <div
+            id="drop-zone"
+            onDrop={dropHandler}
+            onDragOver={dragOverHandler}>
+            <p className="drop-zone-description">Drag & Drop Here Or <b>Browse</b></p>
+          </div>
         </div>
         <input type="file" id="select-file-input" />
         <label for="select-file-input" className="select-file-button">Upload Manifest</label>
