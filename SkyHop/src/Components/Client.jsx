17ba7e57dc76a9f722c29ff24d19a18265611d82
jobindnl <jobindnl@gmail.com>
2024-06-26 02:11:13 -0400

    adds dropdowns to testing client

--- SkyHop/src/Components/Client.jsx
+++ SkyHop/src/Components/Client.jsx
@@ -6,6 +6,52 @@ const Client = () => {
       <label for="Single">Single</label>
       <input type="radio" name="drone" value="Multiple" />
       <label for="Multiple">Multiple</label>
+      <div className="client-select-wrapper">
+        <div className="select-row">
+          <p>Testing Center 1</p>
+          <div className="select">
+            <select>
+              <option selected disabled>Select Client:</option>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
+        </div>
+        <div className="select-row">
+          <p>Testing Center 2</p>
+          <div className="select">
+            <select>
+              <option selected disabled>Select Client:</option>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
+        </div>
+        <div className="select-row">
+          <p>Testing Center 3</p>
+          <div className="select">
+            <select>
+              <option selected disabled>Select Client:</option>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
+        </div>
+        <div className="select-row">
+          <p>Testing Center 4</p>
+          <div className="select">
+            <select>
+              <option selected disabled>Select Client:</option>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
