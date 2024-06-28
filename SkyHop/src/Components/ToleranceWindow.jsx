9d698f452102704dd166769f2a40ed0254f0cd9f
jobindnl <jobindnl@gmail.com>
2024-06-27 23:52:56 -0400

    adds state to toggle button.

--- SkyHop/src/Components/ToleranceWindow.jsx
+++ SkyHop/src/Components/ToleranceWindow.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react"
 import clockImage from "../assets/clock.png"
 
 const ToleranceWindow = () => {
+  const [toggled, setToggle] = useState(false)
+  const handleChange = () => {
+    setToggle(!toggled);
+  }
+
   return (
     <div className="tolerance-window-wrapper">
       <p className="tolerance-window-title">Tolerance Window:</p>
       <div className="tolerance-window-body-wrapper">
         <div className="tolerance-window-left">
           <div className="toggle-wrapper">
-            <input type="checkbox" id="switch" class="checkbox" />
+            <input onChange={handleChange} type="checkbox" id="switch" class="checkbox" />
             <label for="switch" class="toggle" />
-            <p className="toggle-state">Toggle ON</p>
+            <p className="toggle-state">Toggle {toggled ? "ON" : "OFF"}</p>
           </div>
         </div>
         <div className="tolerance-window-right">
