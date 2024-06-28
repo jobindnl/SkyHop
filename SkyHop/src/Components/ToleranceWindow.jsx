d038cc9b5c806a683d7f2e4f4b874564a9d765bb
jobindnl <jobindnl@gmail.com>
2024-06-27 23:49:51 -0400

    makes all formatting changes in styles

--- SkyHop/src/Components/ToleranceWindow.jsx
+++ SkyHop/src/Components/ToleranceWindow.jsx
@@ -4,17 +4,19 @@ const ToleranceWindow = () => {
   return (
     <div className="tolerance-window-wrapper">
       <p className="tolerance-window-title">Tolerance Window:</p>
-      <div className="tolerance-window-left">
-        <div className="toggle-wrapper">
-          <input type="checkbox" id="switch" class="checkbox" />
-          <label for="switch" class="toggle" />
-          <p className="toggle-state">Toggle ON</p>
+      <div className="tolerance-window-body-wrapper">
+        <div className="tolerance-window-left">
+          <div className="toggle-wrapper">
+            <input type="checkbox" id="switch" class="checkbox" />
+            <label for="switch" class="toggle" />
+            <p className="toggle-state">Toggle ON</p>
+          </div>
         </div>
-      </div>
-      <div className="tolerance-window-right">
-        <div className="select-tolerance-level">
-          <img src={clockImage} alt="" />
-          <p>Select Tolerance Level</p>
+        <div className="tolerance-window-right">
+          <div className="select-tolerance-level">
+            <img src={clockImage} alt="" />
+            <p>Select Tolerance Level</p>
+          </div>
         </div>
       </div>
     </div>
