fbf7e3d76e90f08f1f9fdf47496003735399c618
jobindnl <jobindnl@gmail.com>
2024-07-01 10:52:32 -0400

    expands drop zone for file upload

--- SkyHop/src/Components/SelectFile.jsx
+++ SkyHop/src/Components/SelectFile.jsx
@@ -21,13 +21,15 @@ const SelectFile = () => {
   return (
     <div className="select-file half-a-border-on-bottom">
       <p className="select-file-title">Select a manifest you would like to import</p>
-      <div className="drop-zone-wrapper">
+      <div 
+        className="drop-zone-wrapper"
+        onDrop={handleDrop}
+        onDragOver={handleDrag}
+      >
         <div className="drop-zone-border">
           <img src={fileImage} id="image-file-upload" className="drop-zone-image" alt="file-icon" />
           <div
             id="drop-zone"
-            onDrop={handleDrop}
-            onDragOver={handleDrag}
             >
             <p className="drop-zone-description">Drag & Drop Here Or <b>Browse</b></p>
           </div>
